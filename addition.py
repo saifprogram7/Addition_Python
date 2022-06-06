1413b6ecbf98a914c9dfdff07f582d29c15f3608
Saif Al Jabry <68194253+saifprogram7@users.noreply.github.com>
2022-06-06 19:41:54 +0100

    Add files via upload

---
+++ addition.py
@@ -0,0 +1,14 @@
+#Example 1
+from math import *
+user1 = int(input("Please enter first number: "))
+user2 = int(input("Please enter second number: "))
+result = user1 + user2
+print(result)
+
+#Example 2
+def addition():
+    value1 = 72
+    value2 = 38
+    result = value1 + value2
+    print(result)
+addition()
